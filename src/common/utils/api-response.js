88be4eea87adcd136c081e33dc1d2893c17d4840
Sachin Kumar <sachinkuamr0082340@gmail.com>
2026-03-26 11:29:06 +0530

    Building a Restful Api for learning purpose

---
+++ src/common/utils/api-response.js
@@ -0,0 +1,26 @@
+class Apiresponse{
+
+    static ok(res, message, data = null){
+        return res.status(200).json({
+            success: true,
+            message,
+            data
+        })
+    }
+
+    static created(res, message, data = null){
+        return res.status(201).json({
+            success: true,
+            message,
+            data
+        })
+    }
+
+    static noContent(res){
+        return res.status(204).send()
+    }
+
+}
+
+
+export default Apiresponse
